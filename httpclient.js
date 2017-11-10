fe98013075987a4ad5780bf944181e12b5ddd34f
stkane <stephenw.kane@gmail.com>
2017-11-10 18:37:08 -0500

    added httpclient.js

---
+++ httpclient.js
@@ -0,0 +1,18 @@
+// Write a program that performs an HTTP GET request to a URL provided to you
+// as the first command-line argument. Write the String contents of each
+// "data" event from the response to a new line on the console (stdout).
+
+var http = require('http');
+var url = process.argv[2];
+
+function theClient(url) {
+	http.get(url, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function(data) {
+			console.log(data);
+		})
+	}); 	
+}
+
+
+theClient(url);
